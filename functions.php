5f14dacf81e891edc330dc5f52f74d50c5b93ab6
Clay Tercek <clay.tercek@gmail.com>
2019-01-06 14:20:21 -0500

    reverted back to acf

--- functions.php
+++ functions.php
@@ -9,11 +9,11 @@ require_once get_parent_theme_file_path('/vendor/autoload.php');
 
 // Plugins
 require get_parent_theme_file_path('includes/timber.php');
+require get_parent_theme_file_path('includes/acf.php');
+
 // Theme
 require get_parent_theme_file_path('includes/assets.php');
 require get_parent_theme_file_path('includes/post-types.php');
-
-require get_parent_theme_file_path('includes/blocks.php');
 
 function register_menus()
 {
---
+++ includes/acf.php
@@ -0,0 +1,34 @@
+<?php
+
+function acf_settings_path($path)
+{
+    $path = get_stylesheet_directory() . '/plugins/acf/';
+    return $path;
+}
+
+function acf_settings_dir($dir)
+{
+    $dir = get_stylesheet_directory_uri() . '/plugins/acf/';
+    return $dir;
+}
+
+/**
+ * Show/Hide ACF fields on dashboard
+ *
+ * @return bool
+ */
+function acf_show_tab()
+{
+    if (getenv('WP_ENV') == 'production') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+add_filter('acf/settings/show_admin', 'acf_show_tab');
+add_filter('acf/settings/path', 'acf_settings_path');
+add_filter('acf/settings/dir', 'acf_settings_dir');
+
+// Include ACF Plugin
+include_once get_stylesheet_directory() . '/plugins/acf/acf.php';
