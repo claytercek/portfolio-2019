69371eac16ee0d835d5791aa6946a9937c933fb4
Clay Tercek <claytercek@Clays-MBP.fios-router.home>
2019-01-10 14:06:03 -0500

    added json sync for field groups

--- includes/acf.php
+++ includes/acf.php
@@ -12,6 +12,24 @@ function acf_settings_dir($dir)
     return $dir;
 }
 
+function my_acf_json_save_point( $path ) {
+    // update path
+    $path = get_stylesheet_directory() . '/plugins/acf-json';
+    // return
+    return $path;
+    
+}
+function my_acf_json_load_point( $paths ) {
+    
+    // remove original path (optional)
+    unset($paths[0]);
+    // append path
+    $paths[] = get_stylesheet_directory() . '/plugins/acf-json';
+    // return
+    return $paths;
+    
+}
+
 /**
  * Show/Hide ACF fields on dashboard
  *
@@ -26,9 +44,19 @@ function acf_show_tab()
     }
 }
 
+add_filter( 'acf/settings/save_json', 'my_acf_json_save_point');
+add_filter( 'acf/settings/load_json', 'my_acf_json_load_point');
+
 add_filter('acf/settings/show_admin', 'acf_show_tab');
 add_filter('acf/settings/path', 'acf_settings_path');
 add_filter('acf/settings/dir', 'acf_settings_dir');
+
+ 
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+ 
+
+
+
 
 // Include ACF Plugin
 include_once get_stylesheet_directory() . '/plugins/acf/acf.php';
