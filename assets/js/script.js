13f15db8747835e71eaa77fe22bd2d5153acdf14
Clay Tercek <clay.tercek@gmail.com>
2019-01-10 10:50:56 -0500

    finished gallery with flickity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var $ = require("jquery");
 var scrollNav = require("./scrollNav");
 var background = require("./background");
+var Flickity = require("flickity");
 
 $(".js-wave-toggleMenu").click(function() {
 	background.toggleMenu();
@@ -13,4 +14,19 @@ $(".js-wave-toggleMenu").click(function() {
 $(document).ready(function() {
 	background.init();
 	scrollNav.init();
+
+	
+	var flkty = new Flickity( ".wp-block-gallery", {
+		// options
+		cellAlign: 'left',
+		contain: false,
+		// disable previous & next buttons and dots
+		prevNextButtons: false,
+		pageDots: true,
+		imagesLoaded: true,
+		cellSelector: 'figure',
+		setGallerySize: false
+	});
+	
+	$(".blocks-gallery-item").remove();
 });
